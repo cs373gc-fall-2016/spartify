b460327ee2a08dea9c75a68179c7722ae03c50ed
Carlos Trevino <carlostrevino@utexas.edu>
2016-10-28 11:55:40 -0500

    Simplified API routes.

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell
 from db import db, app, manager
 from models import Contributor, Paradigm, Language, Project, Company
+import models
 
 """
 """
@@ -14,7 +15,7 @@ from models import Contributor, Paradigm, Language, Project, Company
 
 @app.route('/')
 def index():
-    """ 
+    """
     renders the landing page
     """
     companies = Company.query.all()
@@ -23,7 +24,7 @@ def index():
 
 @app.route('/languages/')
 def lang_temp():
-    """ 
+    """
     renders the languages page
     """
     languages = Language.query.all()
@@ -32,7 +33,7 @@ def lang_temp():
 
 @app.route('/languages/<name>')
 def lang_tmp(name):
-    """ 
+    """
     renders the page for the given language
     """
     language = Language.query.filter_by(name=name.lower()).first()
@@ -41,7 +42,7 @@ def lang_tmp(name):
 
 @app.route('/companies/')
 def comp_temp():
-    """ 
+    """
     renders the companies page
     """
     companies = Company.query.all()
@@ -50,7 +51,7 @@ def comp_temp():
 
 @app.route('/companies/<id>')
 def comp_tmp(id):
-    """ 
+    """
     renders the page for the company with the given id
     """
     company = Company.query.filter_by(id=id).first()
@@ -59,7 +60,7 @@ def comp_tmp(id):
 
 @app.route('/contributors/')
 def contr_temp():
-    """ 
+    """
       renders the contributors page
     """
     contributors = Contributor.query.all()
@@ -68,7 +69,7 @@ def contr_temp():
 
 @app.route('/contributors/<id>')
 def contr_tmp(id):
-    """ 
+    """
     renders the page for the contributor with the given id
     """
     contributor = Contributor.query.filter_by(id=id).first()
@@ -77,7 +78,7 @@ def contr_tmp(id):
 
 @app.route('/projects/')
 def proj_temp():
-    """ 
+    """
     renders the projects page
     """
     projects = Project.query.all()
@@ -86,7 +87,7 @@ def proj_temp():
 
 @app.route('/projects/<id>')
 def proj_tmp(id):
-    """ 
+    """
     renders the page for the project with the given id
     """
     project = Project.query.filter_by(id=id).first()
@@ -95,96 +96,35 @@ def proj_tmp(id):
 
 @app.route('/About/')
 def about_temp():
-    """ 
+    """
     renders the about page
     """
     return render_template('about.html')
 
+resources = {
+    'contributors': 'id',
+    'projects': 'id',
+    'languages': 'name',
+    'companies': 'id'}
 
-@app.route('/api/contributors/')
-def contributors():
-    """ 
-    returns a json list of all contributors 
-    """
-    contributors = Contributor.query.all()
-    return jsonify([create_dict(c) for c in contributors])
-
-
-@app.route('/api/contributors/<id>')
-def contributor(id):
-    """ 
-    returns json for the contributor with the given id 
-    """
-    contributor = Contributor.query.filter_by(id=id).first()
-    if contributor == None:
-        abort(404)
-    return jsonify(create_dict(contributor))
-
-
-@app.route('/api/projects/')
-def projects():
-    """ 
-    returns a json list of all projects 
-    """
-    projects = Project.query.all()
-    return jsonify([create_dict(p) for p in projects])
-
-
-@app.route('/api/projects/<id>')
-def probejct(id):
-    """ 
-    returns json for the project with the given id 
-    """
-    project = Project.query.filter_by(id=id).first()
-    if project == None:
-        abort(404)
-    return jsonify(create_dict(project))
-
-
-@app.route('/api/languages/')
-def languages():
-    """ 
-    returns a json list of all languages
-    """
-    languages = Language.query.all()
-    return jsonify([create_dict(l) for l in languages])
-
-
-@app.route('/api/languages/<name>')
-def language(name):
-    """ 
-    retuns json for the language with the given name 
-    """
-    language = Language.query.filter_by(name=name.lower()).first()
-    if language == None:
-        abort(404)
-    return jsonify(create_dict(language))
-
-
-@app.route('/api/companies/')
-def companies():
-    """ 
-    returns a json list of all companies 
-    """
-    companies = Company.query.all()
-    return jsonify([create_dict(c) for c in companies])
-
-
-@app.route('/api/companies/<id>')
-def company(id):
-    """ 
-    returns json of for the company with the given id 
-    """
-    company = Company.query.filter_by(id=id).first()
-    if company == None:
-        abort(404)
-    return jsonify(create_dict(company))
+@app.route('/api/<resource>/')
+@app.route('/api/<resource>/<id>')
+def resource(resource, id=None):
+    if resource in resources:
+        model = getattr(models, resource[:-1].title())
+        if id is None:
+            return jsonify([create_dict(c) for c in model.query.all()])
+        data = model.query.filter_by(**{resources[resource]: id}).first()
+        if data is None:
+            abort(404)
+        return jsonify(create_dict(data))
+    abort(404)
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
-    """ 
-    error handinging for a 'not found' error 
+    """
+    error handinging for a 'not found' error
     """
     return "Resource not found", 404
 
@@ -205,8 +145,8 @@ manager.add_command('shell', Shell(make_context=shell_context))
 
 
 def create_dict(obj):
-    """ 
-    calls the obj.dictionary() and returns the result 
+    """
+    calls the obj.dictionary() and returns the result
     """
     return obj.dictionary()
 #    return {}
