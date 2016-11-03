0f933f6f38d091de4b3c700fc15fc7fe9818570d
Brian Bechtel <brianbechtel@outlook.com>
2016-11-03 04:32:08 -0500

    Updated things in preparation for submission
    Formatted python files with autopep8
    
    Generated pydoc files for each of the python files
    
    Updated about page statistics
    
    Added link to about page to wiki page that
      explains how data was scraped
    
    Added TA-README file to help TA locate documentation files
    
    Renamed test.sh unit-test.sh as the name test.sh
      was causing weird behavior with make test

--- backend/backend.py
+++ backend/backend.py
@@ -17,49 +17,81 @@ import models
 
 @app.route('/')
 @app.route('/<path:path>')
-def index(path = ""):
+def index(path=""):
     """
-    renders the landing page
+    sends all non defined paths to index.html so
+    Angular2 routing can take over
     """
     return send_from_directory('..', 'index.html')
 
+
 @app.route('/node_modules/<path:path>')
 def send_nodemodules(path):
+    """
+    path to Angular2 javascript libaries
+    """
     return send_from_directory('../node_modules', path)
+
 
 @app.route('/app/<path:path>')
 def send_app(path):
+    """
+    path to frontend static files such as /app/css/app.css
+    """
     return send_from_directory('../app', path)
+
 
 @app.route('/favicon.ico')
 def send_favicon():
+    """
+    path to favicon.ico
+    """
     return send_from_directory('..', 'favicon.ico')
+
 
 @app.route('/system-config.js')
 def send_systemconfig():
+    """
+    path to system-config.js. this file tells the Angular2
+    front-end where to look for the javascript files.
+    """
     return send_from_directory('..', 'system-config.js')
+
 
 @app.route('/system-config.js.map')
 def send_systemconfig_map():
+    """
+    realted to system-config.js. helps the front-end locate
+    required javascript files.
+    """
     return send_from_directory('..', 'system-config.js.map')
+
 
 @app.route('/runtests/')
 def run_unit_tests():
-    subprocess.check_output(["bash", "test.sh"])
+    """
+    path to run unit tests.
+    """
+    subprocess.check_output(["bash", "unit-test.sh"])
     return send_from_directory('.', 'tests.html')
 
 db_by_name = {
-        "contributors": Contributor,
-        "projects": Project,
-        "languages": Language,
-        "companies": Company
-        }
+    "contributors": Contributor,
+    "projects": Project,
+    "languages": Language,
+    "companies": Company
+}
+
 
 @app.route('/api/count/<db_name>')
 def model_count(db_name):
+    """
+    return a count of how many items are in the database
+    """
     if (db_name not in db_by_name):
         abort(404)
     return str(db_by_name[db_name].query.count())
+
 
 @app.route('/api/<db_name>/')
 def model_page(db_name, start=None, end=None, orderby=None, descending=None):
@@ -80,16 +112,20 @@ def model_page(db_name, start=None, end=None, orderby=None, descending=None):
         if db_name not in db_by_name:
             abort(404)
         if (orderby != None and descending == "true"):
-            result = db_by_name[db_name].query.order_by(desc(orderby)).limit(int(end) - int(start)).offset(int(start)).all()
+            result = db_by_name[db_name].query.order_by(desc(orderby)).limit(
+                int(end) - int(start)).offset(int(start)).all()
             return jsonify([x.dictionary() for x in result])
         elif (orderby != None):
-            result = db_by_name[db_name].query.order_by(orderby).limit(int(end) - int(start)).offset(int(start)).all()
+            result = db_by_name[db_name].query.order_by(orderby).limit(
+                int(end) - int(start)).offset(int(start)).all()
             return jsonify([x.dictionary() for x in result])
         else:
-            result = db_by_name[db_name].query.limit(int(end) - int(start)).offset(int(start)).all()
+            result = db_by_name[db_name].query.limit(
+                int(end) - int(start)).offset(int(start)).all()
             return jsonify([x.dictionary() for x in result])
     else:
         abort(404)
+
 
 @app.route('/api/<db_name>/<id>')
 def single_model(db_name, id):
@@ -103,12 +139,14 @@ def single_model(db_name, id):
         abort(404)
     return jsonify(ret.dictionary())
 
+
 @app.errorhandler(404)
 def resource_not_found(e):
     """
     error handinging for a 'not found' error
     """
     return "Resource not found", 404
+
 
 def shell_context():
     context = {
